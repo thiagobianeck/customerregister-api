e26d9bb276bdb88819561e7826d4d66a5d6eb35f
Thiago Moreira Bianeck <thiagobianeck@gmail.com>
2020-12-10 17:48:47 -0300

    Commit inicial

---
+++ src/main/java/com/bianeck/customerregisterapi/CustomerregisterApiApplication.java
@@ -0,0 +1,13 @@
+package com.bianeck.customerregisterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerregisterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerregisterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bianeck/customerregisterapi/CustomerregisterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bianeck.customerregisterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerregisterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
