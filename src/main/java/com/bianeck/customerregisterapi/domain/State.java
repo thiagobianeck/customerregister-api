0db8a4946b83654d1c23a44a5bafd1cfe72558b7
Thiago Moreira Bianeck <thiagobianeck@gmail.com>
2020-12-14 11:22:58 -0300

    Adjusting models to specification

--- src/main/java/com/bianeck/customerregisterapi/domain/State.java
+++ src/main/java/com/bianeck/customerregisterapi/domain/State.java
@@ -18,14 +18,18 @@ public class State extends Auditable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String name;
+    @Column(nullable = false, unique = true, length = 2)
+    private String uf;
+
+    @Column(nullable = false, unique = true, length = 25)
+    private String description;
 
     @OneToMany(mappedBy = "state")
     private List<City> cities = new ArrayList<>();
 
-    public State(Long id, String name) {
+    public State(Long id, String uf, String name) {
         this.id = id;
-        this.name = name;
+        this.uf = uf;
+        this.description = name;
     }
 }
